0dd720e514fbb6ae760a0dab47ddd9190f7051fd
Đức Bảo <ndbpro99@gmail.com>
2020-12-07 18:53:44 +0700

    .

--- DoAnNam2/Entities/Nhanvien.cs
+++ DoAnNam2/Entities/Nhanvien.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace DoAnNam2.Entities
 {
-     public class Nhanvien
+    public class Nhanvien
     {
-        string manv;
-        string tennv, gioitinh;
-        DateTime ngaysinh;
-        double luong;
+        private string manv;
+        private string tennv, gioitinh;
+        private DateTime ngaysinh;
+        private int luongcoban = 3000000;
+        private double hsl;
         private Nhanvien nhanvien;
 
         public Nhanvien()
@@ -28,14 +29,26 @@ namespace DoAnNam2.Entities
             this.tennv = Tennv;
             this.gioitinh = Gioitinh;
             this.ngaysinh = Ngaysinh;
-            this.luong = luong;
+            this.hsl = HSL;
         }
 
         public string Manv { get => manv; set => manv = value; }
         public string Tennv { get => tennv; set => tennv = value; }
         public string Gioitinh { get => gioitinh; set => gioitinh = value; }
         public DateTime Ngaysinh { get => ngaysinh; set => ngaysinh = value; }
-        public double Luong { get => luong; set => luong = value; }
+        public double HSL
+        {
+            get { return hsl; }
+            set
+            {
+                if (value > 0)
+                    hsl = value;
+            }
+        }
+        public double Tinhluong
+        {
+            get { return luongcoban * HSL; }
+        }
 
         internal static List<Nhanvien> TimNhanVien(Nhanvien nhanvien)
         {
--- DoAnNam2/Presenation/frmNhanvien.cs
+++ DoAnNam2/Presenation/frmNhanvien.cs
@@ -16,13 +16,16 @@ namespace DoAnNam2.Presenation
             public void Nhap()
             {
                 Console.Clear();
-                Console.WriteLine("NHAP THONG TIN NHAN VIEN");
+                Console.SetCursorPosition(25, 5); Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.SetCursorPosition(25, 6); Console.WriteLine("|                        Them thong tin nhan vien                            |");
+                Console.SetCursorPosition(25, 7); Console.WriteLine("|----------------------------------------------------------------------------|");
+                Console.SetCursorPosition(25, 19); Console.WriteLine("----------------------------------------------------------------------------");
                 Nhanvien nv = new Nhanvien();
-                Console.Write("Nhap ma nhan vien:"); nv.Manv = Console.ReadLine();
-                Console.Write("Nhap ten nhan vien:"); nv.Tennv = Console.ReadLine();
-                Console.Write("Nhap ngay sinh:"); nv.Ngaysinh = DateTime.Parse(Console.ReadLine());
-                Console.Write("Nhap gioi tinh:"); nv.Gioitinh = Console.ReadLine();
-                Console.Write("Nhap luong:"); nv.Luong = double.Parse(Console.ReadLine());
+                Console.SetCursorPosition(27, 8); Console.Write("Nhap ma nhan vien:"); nv.Manv = Console.ReadLine();
+                Console.SetCursorPosition(27, 10); Console.Write("Nhap ten nhan vien:"); nv.Tennv = Console.ReadLine();
+                Console.SetCursorPosition(27, 12); Console.Write("Nhap ngay sinh:"); nv.Ngaysinh = DateTime.Parse(Console.ReadLine());
+                Console.SetCursorPosition(27, 12); Console.Write("Nhap gioi tinh:"); nv.Gioitinh = Console.ReadLine();
+                Console.SetCursorPosition(27, 14); Console.Write("Nhap  he so luong:"); nv.HSL = double.Parse(Console.ReadLine());
                 nvDLL.ThemNhanVien(nv);
             }
             public void Hien()
@@ -30,16 +33,25 @@ namespace DoAnNam2.Presenation
                 Console.Clear();
                 Console.WriteLine("HIEN THI THONG TIN NHAN VIEN");
                 List<Nhanvien> list = nvDLL.GetAllNhanvien();
+                Console.SetCursorPosition(9, 5); Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+                Console.SetCursorPosition(9, 6); Console.WriteLine("|                                        Hien thong tin nhan vien                                       |");
+                Console.SetCursorPosition(9, 7); Console.WriteLine("|-------------|----------------------|-----------------|--------------------|--------------|------------|");
+                Console.SetCursorPosition(9, 8); Console.WriteLine("| Ma nhan vien|    Ten nhan vien     |    Gioi tinh    |     Ngay sinh      |   He so luong|  Tien luong |");
+                Console.SetCursorPosition(9, 9); Console.WriteLine("|-------------|----------------------|-----------------|--------------------|--------------|-------------|");
                 foreach (var nv in list)
-                    Console.WriteLine(nv.Manv + "\t" + nv.Tennv + "\t" + nv.Ngaysinh + "\t" + nv.Gioitinh + "\t" + nv.Luong);
+                    Console.WriteLine("\t |" + nv.Manv + "\t |   " + nv.Tennv + "\t |           |  " + nv.HSL + "\t |  " + nv.Tinhluong + "\t | ");
+                Console.Write("          Nhan phim bat ki de tiep tuc...");
             }
             public void Sua()
             {
                 Console.Clear();
-                Console.WriteLine("SUA THONG TIN NHAN VIEN");
+                Console.SetCursorPosition(25, 5); Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.SetCursorPosition(25, 6); Console.WriteLine("|                          Sua thong tin nhan vien                          |");
+                Console.SetCursorPosition(25, 7); Console.WriteLine("|---------------------------------------------------------------------------|");
+                Console.SetCursorPosition(25, 20); Console.WriteLine("-----------------------------------------------------------------------------");
                 List<Nhanvien> list = nvDLL.GetAllNhanvien();
                 string Tensua;
-                Console.Write("NHAP TEN NHAN VIEN CAN SUA:");
+                Console.SetCursorPosition(27, 9); Console.Write("NHAP TEN NHAN VIEN CAN SUA:");
                 Tensua = Console.ReadLine();
                 int i = 0;
                 for (i = 0; i < list.Count; ++i)
@@ -49,115 +61,140 @@ namespace DoAnNam2.Presenation
                         if (i < list.Count)
                         {
                             Nhanvien nv = new Nhanvien(list[i]);
-                            Console.Write("Nhap ten moi :");
+                            Console.SetCursorPosition(27, 17); Console.Write("Nhap ten moi :");
                             string ten = Console.ReadLine();
                             if (!string.IsNullOrEmpty(ten)) nv.Tennv = ten;
-                            Console.Write("Nhap ma nhan vien moi:");
+                            Console.SetCursorPosition(27, 17); Console.Write("Nhap ma nhan vien moi:");
                             string ma = Console.ReadLine();
                             if (!string.IsNullOrEmpty(ten)) nv.Manv = ma;
-                            Console.Write("Nhap ngay sinh:");
+                            Console.SetCursorPosition(27, 17); Console.Write("Nhap ngay sinh:");
                             DateTime ngay = DateTime.Parse(Console.ReadLine());
+                            Console.SetCursorPosition(27, 17); Console.Write("Nhap he so luong :");
+                            int hsL = int.Parse(Console.ReadLine());
+                            if (hsL > 0 && hsL <= 30) nv.HSL = hsL;
 
                         }
                     }
                     else
                     {
-                        Console.WriteLine("khong ton tai ten nhan vien nay");
+                        Console.SetCursorPosition(27, 17); Console.WriteLine("khong ton tai ten nhan vien nay");
                     }
                 }
             }
             public void Xoa()
             {
                 Console.Clear();
-                Console.WriteLine("Xoa thong tin nhan vien");
+                Console.SetCursorPosition(25, 5); Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.SetCursorPosition(25, 6); Console.WriteLine("|                           Xoa thong tin nhan vien                          |");
+                Console.SetCursorPosition(25, 7); Console.WriteLine("|----------------------------------------------------------------------------|");
+                Console.SetCursorPosition(25, 15); Console.WriteLine("-----------------------------------------------------------------------------");
                 List<Nhanvien> list = nvDLL.GetAllNhanvien();
                 string Maxoa;
-                Console.Write("Nhap ma nhan vien can xoa:");
+                Console.SetCursorPosition(27, 17); Console.Write("Nhap ma nhan vien can xoa:");
                 Maxoa = Console.ReadLine();
                 int i;
                 for (i = 0; i < list.Count; ++i)
                     if (list[i].Manv == Maxoa) break;
                 if (i < list.Count)
                 {
-                    Console.Write("Xoa thanh cong.....");
+                    Console.SetCursorPosition(27, 17); Console.Write("Xoa thanh cong.....");
                     nvDLL.XoaNhanVien(Maxoa);
                     Console.ReadKey();
                 }
                 else
                 {
-                    Console.Write("Khong ton tai ma nhan vien nay!");
+                    Console.SetCursorPosition(27, 17); Console.Write("Khong ton tai ma nhan vien nay!");
                     Console.ReadKey();
                 }
             }
             public void Tim()
             {
                 Console.Clear();
+                Console.SetCursorPosition(9, 5); Console.WriteLine("--------------------------------------------------------------------------------------------------------");
+                Console.SetCursorPosition(9, 6); Console.WriteLine("|                                Tim kiem thong tin nhan vien                                           |");
+                Console.SetCursorPosition(9, 7); Console.WriteLine("|-------------------------------------------------------------------------------------------------------|");
                 List<Nhanvien> list = Nhanvien.TimNhanVien(new Nhanvien());
                 string makhach;
-                Console.Write("Nhap ma nhan vien can tim:");
+                Console.SetCursorPosition(27, 17); Console.Write("Nhap ma nhan vien can tim:");
                 makhach = Console.ReadLine();
-                for (int i = 0; i < list.Count; ++i)
+                int i;
+                for (i = 0; i < list.Count; ++i)
+                    if (makhach == list[i].Manv) break;
+                if (i < list.Count)
                 {
-                    if (makhach == list[i].Manv)
-                        Console.WriteLine(list[i].Manv + "\t" + list[i].Tennv + "\t" + list[i].Gioitinh + "\t" + list[i].Ngaysinh + "\t" + list[i].Luong);
+                    Console.SetCursorPosition(9, 9); Console.WriteLine("|                                     Thong tin nhan vien                                            |");
+                    Console.SetCursorPosition(9, 10); Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                    Console.SetCursorPosition(9, 11); Console.WriteLine("║ Ma nhan vien |    Ten nhan vien      |    Gioi tinh   |      Ngay sinh   |    He so luong   |  Tien luong|");
+                    Console.SetCursorPosition(9, 12); Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+                    Console.SetCursorPosition(9, 13); Console.WriteLine("| " + list[i].Manv + "\t |   " + list[i].Tennv + "|  \t" + list[i].HSL + "\t |  " + list[i].Tinhluong + "\t | ");
+                    Console.SetCursorPosition(9, 14); Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
+                    Console.SetCursorPosition(9, 15); Console.Write(" Nhap phim bat ki de tiep tuc");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.SetCursorPosition(9, 11); Console.Write("  Khong ton tai ma nhan vien nay....");
+                    Console.SetCursorPosition(9, 15); Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+                    Console.SetCursorPosition(47, 11);
+                    Console.ReadKey();
                 }
             }
-            public void Menu()
+            public void Menunv()
             {
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine("QUAN LY THONG TIN NHAN VIEN");
-                    Console.WriteLine(" F1.Nhap nhan vien ");
-                    Console.WriteLine(" F2.Sua nhan vien ");
-                    Console.WriteLine(" F3.Xoa nhan vien");
-                    Console.WriteLine(" F4.Hien danh sach ");
-                    Console.WriteLine(" F5.Tim kiem ");
-                    Console.WriteLine(" F6.Back ");
+                    Console.SetCursorPosition(20, 5); Console.Write("-----------------------------------------------------------------------------");
+                    Console.SetCursorPosition(20, 6); Console.Write("|                              Quan Li Nhan Su                              |");
+                    Console.SetCursorPosition(20, 7); Console.Write("|---------------------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 8); Console.Write("|                          QUAN LI THONG TIN NHAN VIEN                      |");
+                    Console.SetCursorPosition(20, 9); Console.Write("|---------------------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 10); Console.Write("|     1    |            Them thong tin nhan vien                           |");
+                    Console.SetCursorPosition(20, 11); Console.Write("|----------|--------------------------------------------------------------- |");
+                    Console.SetCursorPosition(20, 12); Console.Write("|     2    |           Sua thong tin nhan vien                              |");
+                    Console.SetCursorPosition(20, 13); Console.Write("|----------|----------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 14); Console.Write("|     3    |            Xoa thong tin nhan vien                             |");
+                    Console.SetCursorPosition(20, 15); Console.Write("|----------|----------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 16); Console.Write("|     4    |           Hien thong tin nhan vien                             |");
+                    Console.SetCursorPosition(20, 17); Console.Write("|----------|----------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 18); Console.Write("|     5    |            Tim kiem thong tin nhan vien                        |");
+                    Console.SetCursorPosition(20, 19); Console.Write("|----------|----------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 20); Console.Write("|     6    |             Quay lai                                           |");
+                    Console.SetCursorPosition(20, 21); Console.Write("|----------|----------------------------------------------------------------|");
+                    Console.SetCursorPosition(20, 22); Console.Write("|  Moi ban chon lai chuc nang :                                             |");
+                    Console.SetCursorPosition(20, 23); Console.Write("|---------------------------------------------------------------------------|");
+                    Console.SetCursorPosition(48, 22);
                     ConsoleKeyInfo kt = Console.ReadKey();
-                    switch (kt.Key)
+                    switch (kt.KeyChar)
                     {
-                        case ConsoleKey.F1:
+                        case '1':
                             Nhap();
                             Hien();
-                            Console.WriteLine("Nhap phim bat ky de tiep tuc...");
                             Console.ReadKey();
                             break;
-                        case ConsoleKey.F2:
+                        case '2':
                             Sua();
                             Hien();
-                            Console.WriteLine("Nhap phim bat ky de tiep tuc...");
                             Console.ReadKey();
                             break;
-                        case ConsoleKey.F3:
+                        case '3':
                             Xoa();
                             Hien();
-                            Console.WriteLine("Nhan phim bat ki de tiep tuc");
                             Console.ReadKey();
                             break;
-                        case ConsoleKey.F4:
+                        case '4':
                             Hien();
-                            Console.WriteLine("Nhap phim bat ky de tiep tuc...");
                             Console.ReadKey();
                             break;
-                        case ConsoleKey.F5:
+                        case '5':
                             Tim();
-                            Console.WriteLine("Nhan phim bat ki de tiep tuc");
+                            Hien();
                             Console.ReadKey();
-                            break;
-                        case ConsoleKey.F6:
-                            Program.Menu();
-                            break;
+                            break;                        
                     }
                 } while (true);
             }
         }
-
-        internal void Menu()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
-
         
